7a3b289102e54901c7017ec9e3ff9bf3c8401aa7
Jay Bosamiya <jaybosamiya@gmail.com>
2018-01-16 16:05:57 -0500

    Nice shortcut to pop back

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -207,6 +207,7 @@
 	    (add-hook 'fstar-newline-hook
 		      (lambda (ignored-arg) (ocp-indent-line)) nil t)
 	    (add-hook 'before-save-hook 'ocp-indent-buffer nil t)
+	    (local-set-key (kbd "M-,") 'pop-global-mark) ; works nicely with M-.
 	    (local-set-key (kbd "C-c <tab>") 'fstar-indent-subp)))
 
 ;; Set up markdown editing
