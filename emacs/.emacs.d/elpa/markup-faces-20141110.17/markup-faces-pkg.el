e431d9a0de661734b8968361863ef02e9c0c99af
Jay Bosamiya <jaybosamiya@gmail.com>
2018-03-15 18:32:28 -0400

    Remove elpa from being stored in this repo

--- emacs/.emacs.d/elpa/markup-faces-20141110.17/markup-faces-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "markup-faces" "20141110.17" "collection of faces for markup language modes" 'nil :commit "98a807ed82473eb41c6a201ed7ef816d6bcd67b0" :url "https://github.com/sensorflo/markup-faces" :keywords '("wp" "faces"))
--- emacs/.emacs.d/elpa/quick-peek-20170327.27/quick-peek-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "quick-peek" "20170327.27" "Inline quick-peek windows" '((emacs "24.3")) :commit "6a006fbf1a41b04c0e7522d50c942ec89c3637fc" :keywords '("tools" "help" "doc" "convenience"))
