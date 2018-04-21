eb91406f91fcd75a6909ddb2ce257584dbd23d57
Jay Bosamiya <jaybosamiya@gmail.com>
2018-04-21 16:10:14 -0400

    Handle escape sequence colorization better in compilation-mode, shell etc

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -345,3 +345,49 @@ a pulse"
 
 ;; Turn on line numbers for all buffers
 (global-linum-mode)
+
+;; Handle escape sequence colorization properly for compilation-mode
+;; See : https://emacs.stackexchange.com/a/38531
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; Fix escape sequence issues in shell, compilation-mode etc.
+;; See : https://emacs.stackexchange.com/a/38531
+(defun regexp-alternatives (regexps)
+  "Return the alternation of a list of regexps."
+  (mapconcat (lambda (regexp)
+               (concat "\\(?:" regexp "\\)"))
+             regexps "\\|"))
+(defvar non-sgr-control-sequence-regexp nil
+  "Regexp that matches non-SGR control sequences.")
+(setq non-sgr-control-sequence-regexp
+      (regexp-alternatives
+       '(;; icon name escape sequences
+         "\033\\][0-2];.*?\007"
+         ;; non-SGR CSI escape sequences
+         "\033\\[\\??[0-9;]*[^0-9;m]"
+         ;; noop
+         "\012\033\\[2K\033\\[1F"
+         )))
+(defun filter-non-sgr-control-sequences-in-region (begin end)
+  (save-excursion
+    (goto-char begin)
+    (while (re-search-forward
+            non-sgr-control-sequence-regexp end t)
+      (replace-match ""))))
+(defun filter-non-sgr-control-sequences-in-output (ignored)
+  (let ((start-marker
+         (or comint-last-output-start
+             (point-min-marker)))
+        (end-marker
+         (process-mark
+          (get-buffer-process (current-buffer)))))
+    (filter-non-sgr-control-sequences-in-region
+     start-marker
+     end-marker)))
+(add-hook 'comint-output-filter-functions
+          'filter-non-sgr-control-sequences-in-output)
