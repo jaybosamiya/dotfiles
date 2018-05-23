7451a76d7fb481e331961f9aaadbbe5e430019ba
Jay Bosamiya <jaybosamiya@gmail.com>
2018-05-23 12:46:40 -0700

    Finish off install.py, so that it can actually add to dconf

--- dconf/install.py
+++ dconf/install.py
@@ -1,9 +1,14 @@
 from sys import argv, exit
+from os import system
 
 if len(argv) != 2 or argv[1][-len(".dconf"):] != ".dconf":
     print argv
     print ("Usage: %s {name}.dconf" % argv[0])
     exit(1)
 
-# TODO: Actually install :)
-print("Tried to install %s" % repr(argv[1]))
+ret = system('cat "' + argv[1] + '" | dconf load /')
+
+if ret == 0:
+    print("[+] Done installing %s" % repr(argv[1]))
+else:
+    print("[!] Failed installing %s" % repr(argv[1]))
