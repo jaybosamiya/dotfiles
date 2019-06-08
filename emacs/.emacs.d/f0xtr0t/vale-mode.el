07806ee18de8cdfc282b55eca30a4efd8edc76d5
Jay Bosamiya <jaybosamiya@gmail.com>
2019-06-07 17:26:08 -0700

    Add numeric constants

--- emacs/.emacs.d/f0xtr0t/vale-mode.el
+++ emacs/.emacs.d/f0xtr0t/vale-mode.el
@@ -92,6 +92,8 @@
 
      ;; Constants
      ( ,(regexp-opt vale-constants 'symbols) . font-lock-constant-face)
+     ("\\_<\\([0-9]+\\)\\_>" . font-lock-constant-face)
+     ("\\_<\\(0[xX][0-9]+\\)\\_>" . font-lock-constant-face)
 
      ;; Definitions/Declarations
      ("procedure \\_<\\([^ ]*?\\)\\_>" . (1 font-lock-function-name-face))
