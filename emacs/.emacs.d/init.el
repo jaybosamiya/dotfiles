581b1f17e6b08ab8fdf09b25c9daeefaadfdd0bd
Jay Bosamiya <jaybosamiya@gmail.com>
2019-09-04 21:53:53 -0400

    Define as hook so that it runs at the right time

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -340,23 +340,27 @@
 ;; Set up Dafny integration
 (use-package boogie-friends
   :mode ("\\.dfy\\'" . dafny-mode)
+  :init
+  (add-hook 'dafny-mode-hook
+            (lambda ()
+              (setq imenu-generic-expression
+                    '((nil "\\_<datatype\\_> +\\(\\_<.*?\\_>\\) " 1)
+                      (nil "\\_<type\\_> +\\(\\_<.*?\\_>\\) " 1)
+                      (nil "\\_<class\\_> +\\(\\_<.*?\\_>\\) " 1)
+                      (nil "\\_<function method\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
+                      (nil "\\_<function\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
+                      (nil "\\_<method\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
+                      (nil "\\_<predicate\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
+                      (nil "\\_<lemma\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)))
+              (local-set-key (kbd "M-.") 'imenu)
+              (electric-indent-mode -1)))
   :config
   (setq dafny-prefix-path "/opt/dafny/Binaries/")
   (setq flycheck-dafny-executable (concat dafny-prefix-path "dafny"))
   (setq flycheck-z3-executable (concat dafny-prefix-path "z3/bin/z3"))
   (setq dafny-prover-background-args '("/timeLimit:30" "/autoTriggers:1" "/printTooltips"))
   (setq flycheck-inferior-dafny-executable (concat
-					    dafny-prefix-path "dafny-server"))
-  (setq imenu-generic-expression 
-        '((nil "\\_<datatype\\_> +\\(\\_<.*?\\_>\\) " 1)
-          (nil "\\_<type\\_> +\\(\\_<.*?\\_>\\) " 1)
-          (nil "\\_<class\\_> +\\(\\_<.*?\\_>\\) " 1)
-          (nil "\\_<function method\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
-          (nil "\\_<function\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
-          (nil "\\_<method\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
-          (nil "\\_<predicate\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)
-          (nil "\\_<lemma\\_> +?\\({:.*?}\\)? +\\(\\_<.*?\\_>\\) ?(" 2)))
-  (electric-indent-mode -1))
+					    dafny-prefix-path "dafny-server")))
 
 ;; Set up F* integration
 (require 'fstar-mode)
