91b96fe238e20ddce0f3e614abf2e448defebd98
Jay Bosamiya <jaybosamiya@gmail.com>
2019-09-10 14:07:24 -0400

    Make it easy to jump to errors from compilation mode for dafny

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -368,7 +368,10 @@
   (setq flycheck-z3-executable (concat dafny-prefix-path "z3/bin/z3"))
   (setq dafny-prover-background-args '("/timeLimit:30" "/autoTriggers:1" "/printTooltips"))
   (setq flycheck-inferior-dafny-executable (concat
-					    dafny-prefix-path "dafny-server")))
+					    dafny-prefix-path "dafny-server"))
+  (progn ;; Make it easy to jump to errors from compilation mode :)
+    (push 'dafny compilation-error-regexp-alist)
+    (push '(dafny "^\\(.*?\\)(\\([0-9]+\\),\\([0-9]+\\)): " 1 2 3) compilation-error-regexp-alist-alist)))
 
 ;; Set up F* integration
 (require 'fstar-mode)
