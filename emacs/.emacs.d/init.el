4b5c77ce1d2c383b6556e2fbaa3ad751dad69971
Jay Bosamiya <jaybosamiya@gmail.com>
2022-03-04 16:58:55 -0500

    Some nicer default modes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -67,6 +67,7 @@ because otherwise on MacOS, it expands too far and overflows into the notch."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(coq-double-hit-enable t)
  '(custom-enabled-themes '(misterioso))
  '(delete-selection-mode nil)
@@ -74,6 +75,7 @@ because otherwise on MacOS, it expands too far and overflows into the notch."
  '(flyspell-default-dictionary "english")
  '(font-use-system-font t)
  '(global-auto-revert-mode t)
+ '(global-display-line-numbers-mode t)
  '(ido-default-buffer-method 'selected-window)
  '(ido-enable-flex-matching t)
  '(ido-ubiquitous-mode t)
