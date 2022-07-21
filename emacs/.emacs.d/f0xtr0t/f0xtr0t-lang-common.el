dfb7b62c6759c1a400fef186dffc4ca627f3c32a
Jay Bosamiya <jaybosamiya@gmail.com>
2022-07-20 20:43:49 -0400

    Ensure that yasnippets is set up

--- emacs/.emacs.d/f0xtr0t/f0xtr0t-lang-common.el
+++ emacs/.emacs.d/f0xtr0t/f0xtr0t-lang-common.el
@@ -9,4 +9,14 @@
   :ensure t
   :init (dtrt-indent-global-mode 1))
 
+;; Even if we don't use custom snippets, it appears that we need
+;; yas-minor-mode to be set up properly so that company completions
+;; don't end up with the ugly `$0`, `$1` stuff.
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  :hook ((prog-mode . yas-minor-mode)
+         (text-mode . yas-minor-mode)))
+
 (provide 'f0xtr0t-lang-common)
