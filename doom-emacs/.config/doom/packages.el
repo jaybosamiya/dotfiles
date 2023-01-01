24b9587a4bb5a337e9103a31e28824c15ce9400f
Jay Bosamiya <jaybosamiya@gmail.com>
2023-01-01 02:12:49 -0500

    Basic doom-emacs setup for what I want/need

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -1,6 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+;; Disable vertical IDO, since I don't like it. Personal pereference.
+(package! ido-vertical-mode :disable t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
